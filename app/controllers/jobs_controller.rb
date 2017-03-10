bbd9f9aa8c664fa93bb60262e841264ebfb5077f
xurongrong <xurongrong0821@gmail.com>
2017-03-10 08:47:40 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,12 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+    else
+      redirect_to root_path
+    end
   end
 
   def index
